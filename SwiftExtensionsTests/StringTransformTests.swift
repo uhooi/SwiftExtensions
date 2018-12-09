3500c554e9209f4d0225abacb9c09c0cec6804c2
uhooi <uhooi@server.fake>
2018-12-09 14:58:34 +0900

    String+TransformのUTを追加

--- SwiftExtensionsTests/StringTransformTests.swift
+++ SwiftExtensionsTests/StringTransformTests.swift
@@ -67,10 +67,62 @@ class StringTransformTests: XCTestCase {
                                 after: "")
     }
     
-    // MARK: katakanaToHiragana
+    // MARK: halfwidthToFullwidth
     
-    func testHalfwidthToFullwidth() {
-        XCTAssertEqual("ｱｲｳｴｵ".halfwidthToFullwidth(), "アイウエオ")
+    func testHalfwidthToFullwidthKatakana() {
+        checkHalfwidthToFullwidth(before: "ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾜｦﾝｶﾞｷﾞｸﾞｹﾞｺﾞｻﾞｼﾞｽﾞｾﾞｿﾞﾀﾞﾁﾞﾂﾞﾃﾞﾄﾞﾊﾞﾋﾞﾌﾞﾍﾞﾎﾞﾊﾟﾋﾟﾌﾟﾍﾟﾎﾟｳﾞｧｨｩｪｫｯｬｭｮ",
+                                  after: "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨワヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポヴァィゥェォッャュョ")
+    }
+    
+    func testHalfwidthToFullwidthAlphabet() {
+        checkHalfwidthToFullwidth(before: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                                  after: "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ")
+    }
+    
+    func testHalfwidthToFullwidthNumber() {
+        checkHalfwidthToFullwidth(before: "1234567890",
+                                  after: "１２３４５６７８９０")
+    }
+    
+    // TODO: testHalfwidthToFullwidthSymbol()
+    
+    func testHalfwidthToFullwidthScramble() {
+        checkHalfwidthToFullwidth(before: "aア1イ★ウ$エ漢字オひらがな１ｱＡ",
+                                  after: "ａア１イ★ウ＄エ漢字オひらがな１アＡ")
+    }
+    
+    func testHalfwidthToFullwidthEmpty() {
+        checkHalfwidthToFullwidth(before: "",
+                                  after: "")
+    }
+    
+    // MARK: fullwidthToHalfwidth
+    
+    func testfullwidthToHalfwidthKatakana() {
+        checkFullwidthToHalfwidth(before: "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨワヲンガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポヴァィゥェォッャュョ",
+                                  after: "ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾜｦﾝｶﾞｷﾞｸﾞｹﾞｺﾞｻﾞｼﾞｽﾞｾﾞｿﾞﾀﾞﾁﾞﾂﾞﾃﾞﾄﾞﾊﾞﾋﾞﾌﾞﾍﾞﾎﾞﾊﾟﾋﾟﾌﾟﾍﾟﾎﾟｳﾞｧｨｩｪｫｯｬｭｮ")
+    }
+    
+    func testfullwidthToHalfwidthAlphabet() {
+        checkFullwidthToHalfwidth(before: "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ",
+                                  after: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    }
+    
+    func testfullwidthToHalfwidthNumber() {
+        checkFullwidthToHalfwidth(before: "１２３４５６７８９０",
+                                  after: "1234567890")
+    }
+    
+    // TODO: testFullwidthToHalfwidthSymbol()
+    
+    func testFullwidthToHalfwidthScramble() {
+        checkFullwidthToHalfwidth(before: "aア1イ★ウ$エ漢字オひらがな１ｱＡ",
+                                  after: "aｱ1ｲ★ｳ$ｴ漢字ｵひらがな1ｱA")
+    }
+    
+    func testFullwidthToHalfwidthEmpty() {
+        checkFullwidthToHalfwidth(before: "",
+                                  after: "")
     }
     
     // MARK: - Private Methods
@@ -81,5 +133,13 @@ class StringTransformTests: XCTestCase {
     
     private func checkKatakanaToHiragana(before: String, after: String) {
         XCTAssertEqual(before.katakanaToHiragana(), after)
+    }
+    
+    private func checkHalfwidthToFullwidth(before: String, after: String) {
+        XCTAssertEqual(before.halfwidthToFullwidth(), after)
+    }
+    
+    private func checkFullwidthToHalfwidth(before: String, after: String) {
+        XCTAssertEqual(before.fullwidthToHalfwidth(), after)
     }
 }
